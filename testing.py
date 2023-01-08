e688863f314679cdfa068cc6754dfcb04feb3fec
DarcieNg <s3865443@rmit.edu.vn>
2023-01-09 02:41:18 +0700

    Anova test: Education levels versus average working hours

--- testing.py
+++ testing.py
@@ -1,32 +1,9 @@
 import random
 
 import pandas as pd
-import numpy as np
 import scipy.stats as stats
-import researchpy as rp
-import matplotlib.pyplot as plt
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
 
 data = pd.read_csv("data_clean/dataclean.csv")
-
-# t-test: Is there familiar between middle age and young age employees in their income? - 2 sample T-test
-u40 = data[(data['Age'] < 40) & (data['Age'] >= 25)]['Income']
-f40 = data[(data['Age'] >= 40) & (data['Age'] <= 65)]['Income']
-
-u40 = u40.values.tolist()
-u40 = random.sample(u40, 600)
-f40 = f40.values.tolist()
-f40 = random.sample(f40, 600)
-
-ttest,pval = stats.ttest_ind(u40,f40,equal_var = False)
-print("ttest",ttest)
-print('p value',format(pval, '.70f'))
-
-if pval <0.05:
-    print("we reject null hypothesis")
-else:
-    print("we accept null hypothesis")
 
 # Q2: Is there any association between mean working hours and income? - 2-sample T-test
 m50 = data[data['Income'] == 1]['Work hours per week']
